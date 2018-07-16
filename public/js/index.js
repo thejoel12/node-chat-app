714e0af2a6d42d9de69d56aff9eb3e48f0071e2b
Joel Borchardt <joelborchardt@Joels-MacBook-Pro.local>
2018-07-16 10:10:44 -0400

    add css for chat page

--- public/js/index.js
+++ public/js/index.js
@@ -34,12 +34,14 @@ socket.on('disconnect', function () {
 
 jQuery('#message-form').on('submit', function (e) {
     e.preventDefault();
+
+    var messageTextbox = jQuery('[name=message]');
   
     socket.emit('createMessage', {
       from: 'User',
-      text: jQuery('[name=message]').val()
+      text: messageTextbox.val()
     }, function () {
-  
+        messageTextbox.val('');
     });
   });
 
@@ -50,13 +52,17 @@ jQuery('#message-form').on('submit', function (e) {
         return alert('Geolocation Not supported by your browser');
     }
 
+    locationButton.attr('disabled', 'disabled').text('Sending location...');
+
     navigator.geolocation.getCurrentPosition(function (position) {
+        locationButton.removeAttr('disabled').text('Send location');
         socket.emit('createLocationMessage', {
             latitude: position.coords.latitude,
             longitude: position.coords.longitude
         });
         
     }, function () {
+        locationButton.removeAttr('disabled').text('Send location');
         alert('unable to fetch location');
     })
 
